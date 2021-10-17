15edc2835a3f2d6c8a1faf7c93abfd58e3e6e1c9
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-17 12:49:35 -0400

    added name to 44

--- exercise44/src/main/java/baseline/Products.java
+++ exercise44/src/main/java/baseline/Products.java
@@ -1,6 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 David Beers
+ */
 package baseline;
 
 public class Products {
+    // if I refactor this it breaks
     private Product[] products;
 
     public Product[] getProducts() {
--- exercise44/src/test/java/baseline/DataReaderTest.java
+++ exercise44/src/test/java/baseline/DataReaderTest.java
@@ -1,13 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 David Beers
+ */
 package baseline;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DataReaderTest {
 
-    // so this method requires user input, meaning a test won't work
+
     @Test
-    void printQueryResults() {
+    void testPrintQueryResults() {
+        // so this method requires user input, meaning a test won't work
     }
 }
